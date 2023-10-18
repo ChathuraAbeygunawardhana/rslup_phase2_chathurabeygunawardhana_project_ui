33f33a0602738c19e6d4e8b0dfd25a2c85c5f30a
unknown <chathuraabeygunawardhana@gmail.com>
2023-10-18 16:47:50 +0530

    initial_commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+toggleDone(_t7: any) {
+
+}
+removeTodo(arg0: any) {
+
+}
+startEditing(_t7: any) {
+
+}
+cancelEditing(_t7: any) {
+
+}
+updateTodo(_t7: any) {
+
+}
+countItemsLeft() {
+
+}
+clearCompleted() {
+
+}
+todos: any;
+newTodoText: any;
+addTodo() {
+
+}
+  message = 'Welcome to the first Angular app!';
+}
